7e7a43706bb08323a2af8d5453a682929e50b6a2
malki <antonimalki1990@gmail.com>
2019-12-17 16:19:11 +0100

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -8,6 +8,10 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { UiModule } from './ui/ui.module';
 
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -23,7 +27,9 @@ import { UiModule } from './ui/ui.module';
     HttpClientModule
 
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'fr-FR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
